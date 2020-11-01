ed8da0f4218d87dc20c4e5d241c3b282c1afa3eb
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2020-11-01 17:56:26 -0300

    organizando arquivos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Landing from "./components/Landing";
+import Default from "./components/Default";
+import Restaurants from "./components/Restaurants";
+export default class App extends Component {
+  render() {
+    return (
+      <>
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route path="/restaurants" component={Restaurants} />
+          <Route component={Default} />
+        </Switch>
+      </>
+    );
+  }
 }
-
-export default App;
---
+++ src/components/Restaurants.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Restaurants extends Component {
+    render() {
+        return (
+            <div>
+               <h1>Testing Restaurants</h1> 
+            </div>
+        )
+    }
+}
